8d80ff92d0d4aafa22e94c8d508a91eccae597a7
Varun1911 <varunt298@gmail.com>
2025-07-02 14:53:23 +0530

    Custom Api response and Error handling

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+    .then(() =>
+    {
+        const port = process.env.PORT || 8000;
+        app.listen(port, () =>
+        {
+            console.log(`Server is running at port : ${port}`)
+        });
+    })
+    .catch((err) =>
+    {
+        console.error("MongoDB connection failed - ", err);
+    });
